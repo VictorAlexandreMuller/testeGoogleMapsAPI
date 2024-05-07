5a90cddae545020530fff15e2821837ca1b3cfa9
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-05-07 16:32:24 -0300

    overlay map ok

--- overlayMap.js
+++ overlayMap.js
@@ -20,21 +20,21 @@ buttonTerrain.addEventListener("click", function () {
   map.setMapTypeId("terrain");
 });
 
-class MeuMapa {
+class OverlayMap {
   constructor(tileSize) {
     this.tileSize = tileSize;
-    this.maxZoom = 18;
-    this.name = "Meu Mapa";
-    this.alt = "0 mapa não carregou";
+    // this.maxZoom = 18;
+    // this.name = "Meu Mapa";
+    // this.alt = "0 mapa não carregou";
   }
 
   getTile(coord, zoom, ownerDocument) {
     var div = ownerDocument.createElement("div");
-    div.innerHTML = "Teste Victor";
+    // div.innerHTML = "Teste Victor";
     div.style.width = this.tileSize.width + "px";
     div.style.height = this.tileSize.height + "px";
     div.style.fontSize = "10px";
-    div.style.backgroundColor = "#ddd";
+    // div.style.backgroundColor = "#ddd";
     div.style.borderStyle = "solid";
     div.style.borderWidth = "1px";
     div.style.borderColor = "#333";
@@ -45,16 +45,21 @@ class MeuMapa {
 function initMap() {
   var mapOptions = {
     center: { lat: -23.4698412, lng: -47.4299885 },
-    zoom: 17,
+    zoom: 8,
 
     // define como o mapa será renderizado ao abrir a pagina:
     // roadmap, satellite, hybrid, terrain
-    mapTypeId: "roadmap",
-    mapTypeControlOptions: {
-      mapTypeIds: ["meumapa", "roadmap", "satellite"], //, "hybrid", "terrain"
-    },
+    // mapTypeId: "meumapa",
+    // mapTypeControlOptions: {
+    //   mapTypeIds: ["meumapa", "roadmap", "satellite"], //, "hybrid", "terrain"
+    // },
   };
   map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-  map.mapTypes.set("meumapa", new MeuMapa(new google.maps.Size(256, 256)));
+  map.overlayMapTypes.insertAt(
+    0,
+    new OverlayMap(new google.maps.Size(256, 256))
+  );
+
+  // map.mapTypes.set("meumapa", new MeuMapa(new google.maps.Size(256, 256)));
 }
